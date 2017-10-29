e7da067bfd022545856206296709325667ec6a61
alsiesam <lausam1996@gmail.com>
2017-10-29 15:27:38 +0800

    LocalDB

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -36,25 +36,6 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 		connection.close();
 		return result;
 		*/
-		/*String resultString = "";
-		Connection connection = getConnection();
-		String[] keywords = text.split(" ");
-		int hitpoint = 0;
-		for(String keyword:keywords)
-		{
-			PreparedStatement stmt=connection.prepareStatement("SELECT * FROM faq "
-				+ "WHERE LOWER(keyword) LIKE LOWER( CONCAT('%',?,'%') )");
-			stmt.setString(1,keyword);
-			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				resultString = rs.getString(2);
-			}
-			rs.close();
-			stmt.close();
-			if(!resultString.equals("")) break;
-		}
-		connection.close();
-		return resultString;*/
 		String resultString = "";
 		Connection connection = getConnection();
 		PreparedStatement stmt=connection.prepareStatement("SELECT * FROM faq "
